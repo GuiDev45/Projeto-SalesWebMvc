a4f55d59bd082d42bdb397e2dc62b194c8c3fb1e
GuiDev45 <bortolettodev@gmail.com>
2022-07-22 17:11:46 -0300

    CRUD Scaffolding

---
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Data
+{
+    //EntityFrameWork é a ferramenta de mapeamento objeto relacional para acesso a dados no banco de dados,
+    //e esse EntityFrameWork tabalha com a classe DbContext, e esse SalesWebMvcContext é o contexto desse projeto.
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc.Models.Department> Department { get; set; } = default!;
+    }
+}
