5da2fb6ba96aea4440602b6bc2b679a169d3599e
GuiDev45 <bortolettodev@gmail.com>
2022-07-26 18:56:02 -0300

    Outras entidades e segunda migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -4,5 +4,29 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //Adicionar um vendedor/Sellers
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        //Método para calcular o total de vendas do departamento/Department em x período
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            //Para cada vendedor/Seller na lista, some/Sum.
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,43 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        //Adicionar uma venda na lista de vendas/Sales
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        //Remover uma venda na lista de vendas/Sales
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        //Método para retornar o total de vendas/Sales de um vendedor em x período
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
