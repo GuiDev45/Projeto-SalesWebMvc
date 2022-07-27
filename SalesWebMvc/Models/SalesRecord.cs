6b10f64ca3a876b254bf8adad77539572580dcd7
GuiDev45 <bortolettodev@gmail.com>
2022-07-27 00:09:40 -0300

    Seeding Service - Populando DB

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -14,9 +14,8 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+        public SalesRecord(DateTime date, double amount, SalesStatus status, Seller seller)
         {
-            Id = id;
             Date = date;
             Amount = amount;
             Status = status;
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,28 +1,37 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using SalesWebMvc.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
 
 //Build do banco de dados Sql
 builder.Services.AddDbContext<SalesWebMvcContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("SalesWebMvcContext")
     ));
 
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Bloco para popular o banco de dados.
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedingService.Seed(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
